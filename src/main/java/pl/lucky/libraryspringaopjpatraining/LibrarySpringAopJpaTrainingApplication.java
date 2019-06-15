d019ca3a67226c2cbddb8c7cfda30876ef3b1e97
lucki90 <tomlucki@wp.pl>
2019-06-15 19:32:44 +0200

    init

---
+++ src/main/java/pl/lucky/libraryspringaopjpatraining/LibrarySpringAopJpaTrainingApplication.java
@@ -0,0 +1,13 @@
+package pl.lucky.libraryspringaopjpatraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarySpringAopJpaTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibrarySpringAopJpaTrainingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/lucky/libraryspringaopjpatraining/LibrarySpringAopJpaTrainingApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.lucky.libraryspringaopjpatraining;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LibrarySpringAopJpaTrainingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
